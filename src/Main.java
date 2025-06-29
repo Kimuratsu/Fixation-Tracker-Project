81171440d2a8e111a080396ec44daea1eaecadd8
Kitsu <kitsudevacc@gmail.com>
2025-06-29 21:08:27 +0200

    added text file creation and filling in main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.xml.namespace.QName;
 
 // Stuff below is to be tossed into other classes later
 // Used to get the current time
+import java.io.File;
 import java.time.Instant;
 // Used for the current timezone on this machine
 import java.time.ZoneId;
@@ -10,11 +11,19 @@ import java.time.ZonedDateTime;
 // Standard java package formatter for date and time, full list of options will be left below
 // MOVE TO THE README LATER
 import java.time.format.DateTimeFormatter;
+// An input-output import that will let the program make text files
+import java.io.FileWriter;
+// Same as before but its just there to catch exceptions
+import java.io.IOException;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        ///
+        /// Stuff below is about the timer behaviour
+        ///
+
         Instant startTime = Instant.now();
         //assigning end of the timer, currently hardcoded to 60 seconds
         Instant endTime = startTime.plusSeconds(60);
@@ -35,7 +44,33 @@ public class Main
 
         System.out.println("Starting time is " + startDateTime + " and ending time is " + endDateTime);
 
+        ///
+        /// Stuff below this is about creating a text document in the files with some sample text
+        ///
 
+        //this will attempt to make a new text file with the name "test.txt"
+        //its going to overwrite whatever file was there previously though so would be good to
+        //make the final code always create either a unique seed or just make a new file by incrementing
+        //some number, and then having it read what the highest number is before it does the action
+        //its probably going to be pretty slow but i can afford that for now
+        //if its too cumbersome ill fix it later, i dont [[think]] its unfixable
+        try (FileWriter writer = new FileWriter("test.txt"))
+        {
+            //this is what will be inside the text file
+            writer.write("test2");
+            //and this is just the confirmation in runtime that it was successful
+            System.out.println("File written");
+        }
+        //this is a necessary exception thrower, basically catching if anything unexpected happened
+        //while making the file
+        catch (IOException e)
+        {
+            System.err.println("Error writing to file: " +  e.getMessage());
+        }
+
+
+        /// Stuff below this is just templates for making sure I remember how OOP stuff is even coded
+        /// in Java
         // Basically all the stuff below is just a temporary template for myself to understand
         // Polymorphism, Inheritance, Encapsulation and probably Abstraction within Java
         // It's gonna get removed by the final git commit when I send my project over
