f004acd90b964b28ce386e1729cca58c39a434bd
Kitsu <kitsudevacc@gmail.com>
2025-06-29 23:52:21 +0200

    implemented text file creation as separate class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-// Stuff below is to be tossed into other classes later
 import java.time.Instant;
-// Same as before but its just there to catch exceptions
-import java.io.IOException;
-
-// These three are needed for the code to read the filepaths used for my text document writing solution
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main
 {
@@ -37,43 +29,14 @@ public class Main
         /// Stuff below this is about creating a text document in the files with some sample text
         ///
 
-        //some variables needed for the code below,
-        //specifically file contents
-        String content = "test of what gets written into the NoteStorage directory";
-        //the subdirectory, this one will likely not change for this project
-        String subdirectory = "NoteStorage";
-        //the name of the file
-        String filename = "test.txt";
+        TextFileHandler textFileHandler = new TextFileHandler();
 
-        //this tries to make a text file with name and contents specified above
-        //its going to overwrite whatever file was there previously though so would be good to
-        //make the final code always create either a unique seed or just make a new file by incrementing
-        //some number, and then having it read what the highest number is before it does the action
-        //its probably going to be pretty slow but i can afford that for now
-        //if its too cumbersome ill fix it later, i dont [[think]] its unfixable
-        try
-        {
-            //creates path if needed, just a precaution to not need to make an exception
-            Path dirPath = Paths.get(subdirectory);
-            if(!Files.exists(dirPath))
-            {
-                Files.createDirectory(dirPath);
-            }
-
-            //this creates a file in the specific filepath
-            Path filePath = dirPath.resolve(filename);
-
-            //this writes the contents
-            Files.write(filePath, content.getBytes());
-            System.out.println("File written to " + filePath);
-        }
-        //this is a necessary exception thrower, basically catching if anything unexpected happened
-        //while making the file
-        catch (IOException e)
-        {
-            System.err.println("Error writing to file: " +  e.getMessage());
-        }
-
+        /// This creates a new note in the /NoteStorage subdirectory
+        /// This test currently overwrites the previous note which NEEDS TO BE FIXED VIA ID SYSTEM
+        /// The ID system could either be a specific unique Seed, or simple file incrementation
+        /// The solution present in the final project I send is likely unoptimised, but such is life
+        /// It can and will be fixed, but I couldn't dwell on that detail at the moment
+        textFileHandler.NewNote("Test of what gets written into the subdirectory", "NoteStorage");
 
         /// Stuff below this is just templates for making sure I remember how OOP stuff is even coded
         /// in Java
@@ -86,9 +49,9 @@ public class Main
         System.out.println();
         TherapyCheckup therapyCheckup = new TherapyCheckup();
 
-        MyBraincells braincells = new MyBraincells("Braincells", 4);
-        IQPoints iq = new IQPoints("IQ Points", 8, false);
-        Thoughts thoughts = new Thoughts(1236, "Unrelated", "Thoughts");
+        MyBraincells braincells = new MyBraincells("Braincells", 5);
+        IQPoints iq = new IQPoints("IQ Points", 12, false);
+        Thoughts thoughts = new Thoughts(2738, "Unrelated", "Thoughts");
 
         therapyCheckup.addBraincell(braincells);
         therapyCheckup.addBraincell(iq);
---
+++ src/TextFileHandler.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TextFileHandler
+{
+    public void NewNote(String content, String subdirectory)
+    {
+        //specifically file contents
+        //String content = "test of what gets written into the NoteStorage directory";
+        //the subdirectory, this one will likely not change for this project
+        //String subdirectory = "NoteStorage";
+
+        /// IMPORTANT!!! The name of the file is hardcoded right now and WILL overwrite the previous
+        /// Test file. Fix this by either making the user input the name [undesirable change]
+        /// or by adding that ID system via further inheritance (?) and linking the timers with notes
+        String filename = "test.txt";
+
+        try
+        {
+            /// Creates path if needed, replacement for exception handling
+            Path dirPath = Paths.get(subdirectory);
+            if(!Files.exists(dirPath))
+            {
+                Files.createDirectory(dirPath);
+            }
+
+            /// This creates a file in the specific filepath
+            Path filePath = dirPath.resolve(filename);
+
+            /// This writes the contents
+            Files.write(filePath, content.getBytes());
+            System.out.println("File written to " + filePath);
+        }
+        /// This is a necessary exception thrower, catching if anything unexpected happened
+        /// while making the file
+        catch (IOException e)
+        {
+            System.err.println("Error writing to file: " +  e.getMessage());
+        }
+    }
+}
